7d5fb85e18b514c4c81b519ab05739bfe5d71d49
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-14 11:40:43 +0200

    components adding

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'         //shortcut rce
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>NewsItem</div>
+    )
+  }
+}
+
+export default NewsItem
