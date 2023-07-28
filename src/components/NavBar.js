1a957a5fba5ca9f9e6b27305422d07c1ec3f2e23
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-28 21:35:37 +0200

    About and Contact pages updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ export class NavBar extends Component {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">NewsApp</a>
+          <a class="navbar-brand" href="/">NewsAppUsingAPI</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -18,8 +18,12 @@ export class NavBar extends Component {
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/">About</a>
+                <a class="nav-link" href="/about">About</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+              </li>
+
 
             </ul>
           </div>
