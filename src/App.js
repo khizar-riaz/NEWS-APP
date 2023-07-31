fd438e5637339e4be515b76bcf403d9e93e110cf
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-31 16:11:28 +0200

    Routing of the pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import React, { Component } from 'react'
 import NavBar from './components/NavBar';
-import News from './components/News';
+import News from './components/News'
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 export default class App extends Component {
   render() {
     return (
+      <Router>
       <div>
+       
         <NavBar/>
-        <News/>
+        <Routes>
+        <Route exact path="/" element={<News />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />       
+        </Routes>
       </div>
+      </Router>
     )
   }
 }
