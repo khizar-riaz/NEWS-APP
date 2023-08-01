bdcce8f716bdcaefe1b934affba6888ffa193817
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-08-01 18:40:37 +0200

    NavLink to show as SPA

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types'     //shortcut rcep
 import React, { Component } from 'react'
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
   static propTypes = {}
@@ -8,20 +10,28 @@ export class NavBar extends Component {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">NewsAppUsingAPI</a>
+        <NavLink className="navbar-brand" exact to="/">
+        NewsAppUsingAPI
+        </NavLink>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              <NavLink className="nav-link" aria-current="page" exact to="/">
+                Home
+              </NavLink>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+              <NavLink className="nav-link" exact to="/about">
+                About
+              </NavLink>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
+              <NavLink className="nav-link" exact to="/contact">
+                Contact
+              </NavLink>
               </li>
             </ul>
           </div>
