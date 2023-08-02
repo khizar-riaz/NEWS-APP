3b1bb53cc1ac707c65a9e1db42e90e5f1f869f8e
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-08-02 21:21:38 +0200

    About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About Page</div>
+    <div className="container">
+      <h2 className="py-3">About Page</h2>
+      <p className="lead">
+        Lorem ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups. Lorem
+        ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups. Lorem
+        ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+      <p className="lead">
+        Lorem ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups. Lorem
+        ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups. Lorem
+        ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
   );
 }
 
-export default About
+export default About;
